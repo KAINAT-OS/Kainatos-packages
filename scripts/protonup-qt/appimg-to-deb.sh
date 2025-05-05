4be29d6c21d7c90835ffa5b66d1d35af6c2badff
kainatq <kainatquaderee@gmail.com>
2025-05-05 23:22:20 +0600

    shd

--- scripts/protonup-qt/appimg-to-deb.sh
+++ scripts/protonup-qt/appimg-to-deb.sh
@@ -16,8 +16,7 @@ fi
 PKG_NAME="$(basename "$APPIMAGE" .AppImage)"
 # Try to extract version from AppImage metadata or fallback to timestamp
 VERSION="$(strings "$APPIMAGE" \
-  | grep -m1 -E '^[0-9]+\.[0-9]+(\.[0-9]+)?' \
-  || date +%Y%m%d%H%M%S)"
+  | grep -oP 'ProtonUp-Qt-\K[0-9]+\.[0-9]+\.[0-9]+')"
 ARCH="$(dpkg --print-architecture)"
 
 # Prepare workspace
